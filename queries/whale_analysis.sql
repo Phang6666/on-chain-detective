c28e6ce234aabc9f335e57def297875e4efe983e
Phang6666 <124148754+Phang6666@users.noreply.github.com>
2025-11-24 00:39:00 +0800

    Update whale_analysis.sql

--- queries/whale_analysis.sql
+++ queries/whale_analysis.sql
@@ -1,10 +1,9 @@
 select
-    evt_tx_hash as transaction_hash
-    , "to" as wallet_address
-    , value / 1e18 as amount_bought
+    evt_tx_hash as transaction_hash, 
+    "to" as wallet_address, 
+    value / 1e18 as amount_bought
 from erc20_ethereum.evt_Transfer
 where contract_address = 0x6982508145454Ce325dDbE47a25d4ec3d2311933 -- PEPE Contract Address
-   and evt_block_time > now() - interval '24' hour
-order by
-    amount_bought desc
+and evt_block_time > now() - interval '24' hour
+order by amount_bought desc
 limit 10
